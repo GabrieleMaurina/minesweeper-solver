4e0d0fdf69d56b0d1376944c5f016d674ad44d15
GabrieleMaurina <gabrielemaurina95@gmail.com>
2023-04-14 00:04:45 -0600

    Added svg images.

---
+++ src/consts/images.py
@@ -0,0 +1,31 @@
+from io import BytesIO
+from cairosvg import svg2png
+from PIL import Image
+from os.path import join
+from consts.consts import Consts
+
+
+FOLDER = 'resources/images'
+
+
+class Images:
+    def __init__(self):
+        self.numbers = [self.load_image(str(i)) for i in range(9)]
+        # self.mine = self.load_image('mine')
+        # self.red_mine = self.load_image('red_mine')
+        self.flag = self.load_image('flag')
+        self.questionmark = self.load_image('question_mark')
+        self.covered = self.load_image('covered')
+        self.smile = self.load_image('smile')
+        self.sunglasses = self.load_image('sunglasses')
+        self.dead = self.load_image('dead')
+
+    def load_image(self, name):
+        bytes = BytesIO()
+        svg2png(url=join(FOLDER, name+'.svg'), write_to=bytes)
+        return Image.open(bytes).resize((Consts.CELL_SIZE, Consts.CELL_SIZE))
+
+
+Images = Images()
+
+exit(0)
